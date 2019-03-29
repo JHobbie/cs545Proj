0512979d89dec74792ac0306bceb6f7e64c15081
Caleb Obeng <cobeng@stevens.edu>
2019-03-29 18:52:15 -0400

    more assets

---
+++ FlashMappers/Assets/Scripts/gameRoom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /*
+		By Monday:
+		-person auto-runs?
+		-at intersection, question pops up (one question for now)
+		-person can pick their answer by tapping/clicking
+
+
+		TODO:
+		1. Question pops up at intersection
+		2. Question can be answered through interaction
+		3. Choices randomly associated with direction
+		4. Person goes in direction of choice
+		5. Right answers removed from queue
+		6. Wrong answers are re-entered at the end
+		7. Timer of some kind (count up)
+		8. Music
+    */
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
