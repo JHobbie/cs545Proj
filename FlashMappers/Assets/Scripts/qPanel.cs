7325b315b462c26ecc2d9a2e6b2450ffc3b71118
asingh97 <asingh28@stevens.edu>
2019-04-04 17:23:56 -0400

    some code for displaying text

---
+++ FlashMappers/Assets/Scripts/qPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//This is code to display a question in a UI panel 
+public class qPanel : MonoBehaviour {
+
+    public GameObject questionPanel;
+    public Text questionText;
+    public Text choiceText;
+
+    // Start is called before the first frame update
+    void Start(){
+        questionPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    void DisplayQ (){
+        qPanel.SetActive(true);
+        questionText.text = "question";
+        choiceText.text = "3 choices";
+    }
+    
+}
