55053fa0f05078cf26f6a79546207c3f1b502455
Caleb Obeng <cobeng@stevens.edu>
2019-04-05 18:37:06 -0400

    maybe thisll work

--- FlashMappers/Assets/Scripts/playerMovement.cs
+++ FlashMappers/Assets/Scripts/playerMovement.cs
@@ -7,37 +7,45 @@ public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     //public GameObject player;
-    public bool goingLeft;
-    public bool goingRight;
-    public bool goingUp;
-    public bool goingDown;
+    public static bool fromLeft;
+    public static bool fromRight;
+    public static bool fromTop;
+    public static bool fromBottom;
 
     public Scene currScene;
     void Start()
     {
         currScene = SceneManager.GetActiveScene();
-        goingLeft = true;
-        goingRight = false;
-        goingUp = false;
-        goingDown = false;
+        fromLeft = true;
+        fromRight = false;
+        fromTop = false;
+        fromBottom = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //moving the player until they activate the question
-        if(goingLeft){
-            transform.position = new Vector3(transform.position.x + 0.3f, transform.position.y, transform.position.z);
+        if(fromLeft){
+            for(int i = 0; i < 25; i++){
+                transform.position = new Vector3(transform.position.x + 0.3f, transform.position.y, transform.position.z);
+            }
         }
-        else if(goingRight){
-            transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, transform.position.z);
+        else if(fromRight){
+            for(int i = 0; i < 25; i++){
+                transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, transform.position.z);
+            }
         }
-        else if(goingUp){
-            transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
+        else if(fromBottom){
+            for(int i = 0; i < 15; i++){
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
+            }
         }
         else{
-            transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
+            for(int i = 0; i < 15; i++){
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
+            }
         }
-
     }
+    
 }
