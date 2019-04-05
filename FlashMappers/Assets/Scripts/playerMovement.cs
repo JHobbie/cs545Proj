e240a2a80cc2496bcc98cf6c115829cd0944e931
Caleb Obeng <cobeng@stevens.edu>
2019-04-04 20:39:34 -0400

    moving in directions

--- FlashMappers/Assets/Scripts/playerMovement.cs
+++ FlashMappers/Assets/Scripts/playerMovement.cs
@@ -1,19 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     //public GameObject player;
+    public bool goingLeft;
+    public bool goingRight;
+    public bool goingUp;
+    public bool goingDown;
+
+    public Scene currScene;
     void Start()
     {
-        
+        currScene = SceneManager.GetActiveScene();
+        goingLeft = false;
+        goingRight = false;
+        goingUp = false;
+        goingDown = false;
+        if(currScene.name == "game"){
+            goingLeft = true;
+        }
+        else if(currScene.name == "game2"){
+            goingRight = true;
+        }
+        else if(currScene.name == "game3"){
+            goingUp = true;
+        }
+        else{
+            goingDown = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right;
+        //moving the player until they activate the question
+        if(goingLeft){
+            transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+        }
+        else if(goingRight){
+            transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+        }
+        else if(goingUp){
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+        }
+        else{
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+        }
+
     }
 }
