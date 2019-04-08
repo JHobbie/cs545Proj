711c801dd206e576cbf843fbc1860fd3a37f9b87
Caleb Obeng <cobeng@stevens.edu>
2019-04-07 20:28:03 -0400

    more movement stuff

--- FlashMappers/Assets/Scripts/playerMovement.cs
+++ FlashMappers/Assets/Scripts/playerMovement.cs
@@ -11,6 +11,7 @@ public class playerMovement : MonoBehaviour
     public static bool fromRight;
     public static bool fromTop;
     public static bool fromBottom;
+    private float speeed = 0.2f;
 
     public Scene currScene;
     void Start()
@@ -72,34 +73,37 @@ public class playerMovement : MonoBehaviour
     }
     IEnumerator waitForOne(){
         //moving the player until they activate the question
+
         if(fromLeft){
-            for(float i=transform.position.x; i <= -10.05f; i+=0.05f){
-                if(transform.position.x != -10.05){
-                    transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+            for(float i=transform.position.x; i <= -10.05f; i+=0.2f){
+                if(transform.position.x <= -10.05f){
+                    transform.position = new Vector3(transform.position.x + speeed, transform.position.y, transform.position.z);
                 }
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(0.0051f);
             }
             fromLeft = false;
             //StartCoroutine(waitForAnswer());
         }
         else if(fromRight){
-            while(transform.position.x != 29f){
-                transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+            for(float i=transform.position.x; i <= -29f; i+=0.2f){
+                if(transform.position.x >= -29f){
+                    transform.position = new Vector3(transform.position.x - speeed, transform.position.y, transform.position.z);
+                }
             }
             fromRight = false;
             //StartCoroutine(waitForAnswer());
         }
         else if(fromBottom){
             while(transform.position.y != 14f){
-                transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+                transform.position = new Vector3(transform.position.x, transform.position.y + speeed, transform.position.z);
                 
             }
             fromBottom = false;
             //StartCoroutine(waitForAnswer());
         }
         else if (fromTop){
-            while(transform.position.y != 12.5){
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+            while(transform.position.y != 12.5f){
+                transform.position = new Vector3(transform.position.x, transform.position.y - speeed, transform.position.z);
             }
             fromTop = false;
             //StartCoroutine(waitForAnswer());
