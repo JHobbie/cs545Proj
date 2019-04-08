35c2d47138b4c89a930b0647fc80a76a44218de4
Caleb Obeng <cobeng@stevens.edu>
2019-04-08 19:24:43 -0400

    button work

--- FlashMappers/Assets/Scripts/diamondButtons.cs
+++ FlashMappers/Assets/Scripts/diamondButtons.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class diamondButtons : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static GameObject topButton;
+    public static GameObject leftButton;
+    public static GameObject rightButton;
+    public static GameObject bottomButton;
+
     void Start()
     {
-        
+        topButton = GameObject.FindGameObjectsWithTag("TopButton")[0];
+        bottomButton = GameObject.FindGameObjectsWithTag("BottomButton")[0];
+        rightButton = GameObject.FindGameObjectsWithTag("LeftButton")[0];
+        leftButton = GameObject.FindGameObjectsWithTag("RightButton")[0];
+
+        topButton.SetActive(false);
+        bottomButton.SetActive(false);
+        rightButton.SetActive(false);
+        leftButton.SetActive(false);
+
+        //Debug.Log();
     }
 
     // Update is called once per frame
