c583495d0855e3750ce4b9ded80b5e01d7ce6c3c
Caleb Obeng <cobeng@stevens.edu>
2019-04-19 21:06:03 -0400

    I dont remember

---
+++ FlashMappers/Assets/Scripts/gameTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timer;
+    public string minutes;
+    public string seconds;
+
+    public Text timerText;
+    void Start()
+    {
+        //timerText = GetComponent("Text");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //this is for counting up
+        timer += Time.deltaTime;
+ 
+        minutes = Mathf.Floor(timer / 60).ToString("00");
+        seconds = (timer % 60).ToString("00");
+     
+        //timerPanel.text = minutes + ":" + seconds;
+    }
+}
